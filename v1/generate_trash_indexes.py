538c4ddbb30f33cd89999c19bbf0c54d40a20c79
dheera <dheera@dheera.net>
2018-04-23 00:30:27 +0000

    update

---
+++ v1/generate_trash_indexes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import json
+from nltk.corpus import wordnet
+
+with open('trash_mapping.json', 'r') as f:
+    mapping = json.loads(f.read())
+
+with open('Inception-labels.txt', 'r') as f:
+    labels = [int(l.split(' ')[0].strip('n')) for l in f]
+
+trash_indexes = {}
+
+for trash_category in mapping:
+    print(trash_category)
+    synsets = set()
+    for synset_name in mapping[trash_category]:
+        s = wordnet.synset(synset_name)
+        synsets.add(s.offset())
+        for hyponym in s.hyponyms():
+             synsets.add(hyponym.offset())
+    indexes = []
+    for synset in synsets:
+        if synset in labels:
+            indexes.append(labels.index(synset))
+
+    trash_indexes[trash_category] = indexes
+
+
+with open('trash_indexes.json', 'w') as f:
+    f.write(json.dumps(trash_indexes))
